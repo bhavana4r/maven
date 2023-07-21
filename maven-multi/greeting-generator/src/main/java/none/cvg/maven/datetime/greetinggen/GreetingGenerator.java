77007a4f8077098a8b44cb4f974549e711d9fecf
Bhavana <bhavana.r@pravyuha.com>
2023-07-21 10:39:27 +0530

    change made

--- maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
+++ maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
@@ -3,7 +3,11 @@ package none.cvg.maven.datetime.greetinggen;
 import none.cvg.maven.greeting.Greeting;
 
 public class GreetingGenerator {
-
+    int a=3;
+    int b=5;
+    public void add(){
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
         Greeting greeting = new Greeting();
         System.out.println("\n" + greeting.greet());
