63f9ae2c2efc7c9f22082b943babc697f91ffe1d
Bhavana <bhavana.r@pravyuha.com>
2023-07-21 10:28:17 +0530

    added method

--- maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
+++ maven-multi/greeting-generator/src/main/java/none/cvg/maven/datetime/greetinggen/GreetingGenerator.java
@@ -5,6 +5,11 @@ import none.cvg.maven.greeting.Greeting;
 public class GreetingGenerator {
 
     public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        public void add(){
+            System.out.println(a+b);
+        }
         Greeting greeting = new Greeting();
         System.out.println("\n" + greeting.greet());
     }
